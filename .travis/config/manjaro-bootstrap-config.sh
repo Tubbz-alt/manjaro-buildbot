e00356eaec67a5dc6ad3770c35e94de7a6b71007
Philip <philm@manjaro.org>
2017-06-21 18:26:07 +0200

    [config] modify to systemd

--- .travis/config/manjaro-bootstrap-config.sh
+++ .travis/config/manjaro-bootstrap-config.sh
@@ -2,6 +2,6 @@
 # config.sh: to override built in defaults
 
 EXTRA_PACKAGES+=(git)
-EXTRA_PACKAGES+=(util-linux-nosystemd)
+EXTRA_PACKAGES+=(util-linux)
 EXTRA_PACKAGES+=(haveged) # for pacman-init
 EXTRA_PACKAGES+=(manjaro-tools-pkg sudo gzip)
